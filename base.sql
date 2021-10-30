0a7969da5c4fa9533a28830a7cfa032996f61a7b
AleksanderMasojc <64375647+AleksanderMasojc@users.noreply.github.com>
2021-10-30 11:26:08 +0200

    correct sql (i think)

--- base.sql
+++ base.sql
@@ -1,84 +1,63 @@
-CREATE TABLE Gallery (
-    id int(5),
-    AlbumID int(5),
-    Author varchar(255),
-    Gallery_name varchar(255),
-    Gallery_description varchar(255),
-    Gallery_last_edited date,
-    Created_at date
+create table album
+(
+    id serial,
+    title text,
+    author text,
+    description text,
+    last_edited date,
+    created_at date
 );
 
-CREATE TABLE Album (
-    id int(5),
-    AlbumID int(5),
-    Author varchar(255),
-    Album_name varchar(255),
-    Album_description varchar(255),
-    Album_last_edited date,
-    Created_at date
+create table image
+(
+    id serial,
+    id_album bigint,
+    title text,
+    author text,
+    description text,
+    created_at date
 );
 
-
-CREATE TABLE Images (
-    id int(5),
-    AlbumID int(5),
-    ImageID int(5),
-    PcommentID int(5),
-    TagID int(5),
-    Author varchar(255),
-    Images_name varchar(255),
-    Images_description varchar(255),
-    Created_at date
+create table primary_comment
+(
+    id serial,
+    id_image bigint,
+    title text,
+    author text,
+    content text,
+    created_at date
 );
 
-CREATE TABLE Primary_comment (
-    id int(5),
-    PcommentID int(5),
-    ScommentID int(5),
-    Author varchar(255),
-    Title varchar(255),
-    Content varchar(255),
-    Created_at date
+create table secondary_comment
+(
+    id serial,
+    title text,
+    author text,
+    last_element_added_at date,
+    created_at date
 );
 
-CREATE TABLE Secondary_comment (
-    id int(5),
-    ScommentID int(5),
-    Author varchar(255),
-    Title varchar(255),
-    Content varchar(255),
-    Created_at date
+create table tag
+(
+    id serial,
+    id_primary bigint,
+    title text,
+    author text,
+    content text,
+    created_at date
 );
 
-CREATE TABLE Tag  (
-    id int(5),
-    TagID int(5),
-    Tag_name varchar(255),
-    Author varchar(255),
-    Last_element_added_at date,
-    Created_at date
+create table tag_image
+(
+    id_image bigint,
+    id_tag bigint,
+    PRIMARY KEY(id_image, id_tag)
 );
-SELECT *
-FROM Album
-LEFT JOIN Gallery
-ON Album.AlbumID = Gallery.AlbumID;
 
-SELECT *
-FROM Images
-LEFT JOIN Album
-ON Images.AlbumID = Album.AlbumID;
+create table tag_album
+(
+    id_album bigint,
+    id_tag bigint,
+    PRIMARY KEY(id_album, id_tag)
+);
 
-SELECT *
-FROM Tag
-LEFT JOIN Image
-ON Tag.TagID = Image.TagID;
-
-SELECT *
-FROM Primary_comment
-LEFT JOIN Image
-ON Primary_comment.PcommentID = Image.PcommentID;
-
-SELECT *
-FROM Secondary_comment
-LEFT JOIN Primary_comment
-ON Secondary_comment.ScommentID = Primary_comment.ScommentID;
