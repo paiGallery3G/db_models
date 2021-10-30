df7d51ff0413ad1cafcfc368876197ea7a95dd7a
AleksanderMasojc <64375647+AleksanderMasojc@users.noreply.github.com>
2021-10-30 11:43:28 +0200

    edit v3

--- base.sql
+++ base.sql
@@ -1,48 +1,46 @@
 create table album
 (
-    id serial,
-    title text,
-    author text,
+    id serial PRIMARY KEY,
+    title varchar(64),
+    author varchar(64),
     description text,
-    last_edited date,
     created_at date
 );
 
 create table image
 (
-    id serial,
+    id serial PRIMARY KEY,
     id_album bigint,
-    title text,
-    author text,
+    title varchar(64),
+    author varchar(64),
     description text,
     created_at date
 );
 
 create table primary_comment
 (
-    id serial,
+    id serial PRIMARY KEY,
     id_image bigint,
-    title text,
-    author text,
+    title varchar(64),
+    author varchar(64),
     content text,
     created_at date
 );
 
 create table secondary_comment
 (
-    id serial,
-    title text,
-    author text,
-    last_element_added_at date,
+    id serial PRIMARY KEY,
+    title varchar(64),
+    author varchar(64),
     created_at date
 );
 
 create table tag
 (
-    id serial,
+    id serial PRIMARY KEY,
     id_primary bigint,
-    title text,
-    author text,
+    title varchar(64),
+    author varchar(64),
     content text,
     created_at date
 );
